df9a76915e153f5b4d6c4d0186f0bbc16c478e83
Robert (laptop) <robert.abela.15@um.edu.mt>
2018-01-02 21:37:15 +0100

    version 0.4: - removed clear storage feature - added options page and moved csv entry there - clean up default popup, added button to launch options

---
+++ src/options.js
@@ -0,0 +1,28 @@
+function showMsg(msg) {
+	$("#msgdiv").text(msg);
+	$("#msgdiv").show();
+}
+
+function showTempMsg(msg) {
+	showMsg(msg);
+	$("#msgdiv").delay(750).fadeOut(250);
+}
+
+function saveTimetable() {
+	var csvValue = $("#timetable").val();
+	var object = {'entries': csvValue};
+	chrome.storage.sync.set(object, function() {
+		showTempMsg('Timetable saved');
+	});
+}
+
+function loadSavedOptions() {
+	chrome.storage.sync.get('entries', function (obj) {
+		if (typeof obj.entries !== "undefined")
+			$("#timetable").text(obj.entries);
+		showMsg('Changes are not saved automatically, remember to save your timetable!');
+	});
+}
+
+$(document).ready(loadSavedOptions);
+$('#save').click(saveTimetable);
--- src/popup.js
+++ src/popup.js
@@ -1,43 +1,11 @@
-/*function runCountjs() {
-  	chrome.tabs.executeScript({
-    	file: 'count.js'
-  });
-}*/
 
-function showMsg(msg) {
-	$("#msgdiv").text(msg);
-	$("#msgdiv").show();
-}
-
-function showTempMsg(msg) {
-	showMsg(msg);
-	$("#msgdiv").delay(750).fadeOut(250);
-}
-
-function saveTimetable() {
-	var csvValue = $("#timetable").val();
-	var object = {'entries': csvValue};
-	chrome.storage.sync.set(object, function() {
-		showTempMsg('Timetable saved');
-	});
-}
-
-function clearStorage() {
-	if (confirm('Are you sure you want to delete the timetable?\nIt might be a good idea to keep a backup.')) {
-		chrome.storage.sync.clear(function() {
-			$("#timetable").text('');
-			showTempMsg('Timetable deleted');
-		});
+$('#configure').click(function() {
+	if (chrome.runtime.openOptionsPage) {
+		// New way to open options pages, if supported (Chrome 42+).
+		chrome.runtime.openOptionsPage();
+	} else {
+		// Reasonable fallback.
+		window.open(chrome.runtime.getURL('options.html'));
 	}
-}
-
-//load old entries
-chrome.storage.sync.get('entries', function (obj) {
-	if (typeof obj.entries !== "undefined")
-		$("#timetable").text(obj.entries);
-	showMsg('Changes are not saved automatically and will be lost when the popup is closed!');
 });
 
-
-$('#save').click(saveTimetable);
-$('#clear').click(clearStorage);
