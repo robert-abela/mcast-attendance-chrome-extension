74941b4c74c758a3ee04d9e53733b865e0450c1a
Robert (laptop) <robert.abela.15@um.edu.mt>
2018-01-02 13:35:46 +0100

    added saving/loading/clearing of csv started csv parsing

--- src/content-script.js
+++ src/content-script.js
@@ -1,13 +1,102 @@
-try {
-	var bgColor = 'LightGreen';
-	var submitBtn = $("form :submit");
-	submitBtn.css("background", bgColor);
+var ThemeColor = 'Indigo';
 
+
+
+// ref: http://stackoverflow.com/a/1293163/2343
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray( strData, strDelimiter ){
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
+
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+        (
+            // Delimiters.
+            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+            // Quoted fields.
+            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+            // Standard fields.
+            "([^\"\\" + strDelimiter + "\\r\\n]*))"
+        ),
+        "gi"
+        );
+
+
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+
+
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec( strData )){
+
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[ 1 ];
+
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (
+            strMatchedDelimiter.length &&
+            strMatchedDelimiter !== strDelimiter
+            ){
+
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push( [] );
+
+        }
+
+        var strMatchedValue;
+
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[ 2 ]){
+
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            strMatchedValue = arrMatches[ 2 ].replace(
+                new RegExp( "\"\"", "g" ),
+                "\""
+                );
+
+        } else {
+
+            // We found a non-quoted value.
+            strMatchedValue = arrMatches[ 3 ];
+
+        }
+
+
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[ arrData.length - 1 ].push( strMatchedValue );
+    }
+
+    // Return the parsed data.
+    return( arrData );
+}
+
+
+try {
+	var submitBtn = $("form :submit");
 	var countBtn = document.createElement("input");
 	countBtn.setAttribute("type", "button");
 	countBtn.value = "Count present";
 	countBtn.className = submitBtn.attr('class');
-	countBtn.style.background = bgColor;
+	countBtn.style.color = ThemeColor;
 	countBtn.style.marginRight='6px';
 	countBtn.addEventListener ("click", function() {
 		var elems = document.getElementsByClassName("waspresent").length;
@@ -23,12 +112,25 @@ try {
 	parent.insertBefore(countBtn, submitBtn.get(0));
 
 	var unitLbl = $('label[for="Unit"]');
-	unitLbl.text("Unit (fill next 3)");
-	unitLbl.css('text-decoration', 'underline');
+	unitLbl.text('Unit (fill next 3)');
+	unitLbl.css({
+		'text-decoration': 'underline', 
+		'color': ThemeColor
+	});
 	unitLbl.on('click', function(e) {
-    	$( "#Unit" ).get(0).value = 'ITSFT-406-1502 - Programming Concepts';
-		$( "#Session" ).get(0).value = '08:00';
-		$( "#Duration" ).get(0).value = '2';
+    	$('#Unit').get(0).value = 'ITSFT-406-1502 - Programming Concepts';
+		$('#Session').get(0).value = '08:00';
+		$('#Duration').get(0).value = '2';
+	});
+
+	chrome.storage.sync.get('entries', function (obj) {
+		if (typeof obj.entries === "undefined") {
+    		console.log('Timetable not found in storage');
+		}
+		else {
+			var parsedEntries = CSVToArray(obj.entries);
+			console.log(parsedEntries.length+' timetable entries found in storage');
+		}
 	});
 }
 catch(err) {
--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,33 @@
-
 function runCountjs() {
   	chrome.tabs.executeScript({
     	file: 'count.js'
   });
 }
 
-//document.getElementById('clickme').addEventListener('click', runCountjs);
+function saveTimetable() {
+	var csvValue = document.getElementById('timetable').value;
+	var object = {'entries': csvValue};
+	chrome.storage.sync.set(object, function() {
+		// Notify that we saved.
+		$("#message").text('Timetable saved');
+	});
+}
+
+function clearStorage() {
+	chrome.storage.sync.clear(function() {
+		document.getElementById('timetable').value = '';
+		$("#message").text('Storage cleared');
+	});
+}
+
+//load old entries
+chrome.storage.sync.get('entries', function (obj) {
+	if (typeof obj.entries === "undefined")
+		$("#message").text('Storage empty');
+	else
+		document.getElementById('timetable').value = obj.entries;
+});
+
+document.getElementById('save').addEventListener('click', saveTimetable);
+document.getElementById('clear').addEventListener('click', clearStorage);
+
