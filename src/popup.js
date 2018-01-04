c532c879c3f519eb74dcebafa75fa029c233ce4c
Robert (laptop) <robert.abela.15@um.edu.mt>
2018-01-04 15:33:39 +0100

    code reorg

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,13 @@
+function addEventsHandler() {
+	$('#configure').click(function() {
+		if (chrome.runtime.openOptionsPage) {
+			// New way to open options pages, if supported (Chrome 42+).
+			chrome.runtime.openOptionsPage();
+		} else {
+			// Reasonable fallback.
+			window.open(chrome.runtime.getURL('options.html'));
+		}
+	});
+}
 
-$('#configure').click(function() {
-	if (chrome.runtime.openOptionsPage) {
-		// New way to open options pages, if supported (Chrome 42+).
-		chrome.runtime.openOptionsPage();
-	} else {
-		// Reasonable fallback.
-		window.open(chrome.runtime.getURL('options.html'));
-	}
-});
-
+$(document).ready(addEventsHandler);
